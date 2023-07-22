495437d0f5f070beede40a64c1a721fffd43039d
Sebastian Manco Gomez <sebasmg1393@gmail.com>
2023-07-22 13:05:38 -0500

    seb: done ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 //third Party library
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 
 //components
 import Homepage from './pages/Homepage';
@@ -15,6 +14,7 @@ import City from './components/City';
 import Form from './components/Form';
 import { CitiesProvider } from './contexts/CitiesContext';
 import { AuthProvider } from './contexts/FakeAuhtContex';
+import ProtectedRoute from './pages/ProtectedRoute';
 
 function App() {
   // console.log(cities);
@@ -27,7 +27,14 @@ function App() {
             <Route path="pricing" element={<Pricing />} />
             <Route path="product" element={<Product />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />}></Route>
               <Route
                 path="cities"
--- src/contexts/FakeAuhtContex.jsx
+++ src/contexts/FakeAuhtContex.jsx
@@ -23,7 +23,8 @@ const FAKE_USER = {
   name: 'Miguel',
   email: 'jack@example.com',
   password: 'qwerty',
-  avatar: 'https://i.pravatar.cc/100?u=zz',
+  // avatar: 'https://i.pravatar.cc/100?u=zz',
+  avatar: '../../public/miguel.jpg',
 };
 
 function AuthProvider({ children }) {
