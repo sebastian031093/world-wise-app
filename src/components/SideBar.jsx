a70c9d78f601b05d23329553c9bb7b40f3ae4e1b
Sebastian Manco G <sebasmg1393@gmail.com>
2025-05-02 10:20:15 -0500

    sebastian manco: file name changed

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import AppNav from './AppNav';
 import Logo from './Logo';
-
+//new name.
 import styles from './SideBar.module.css';
 // TODO:create a new footer component
 function SideBar() {
